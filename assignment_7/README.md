f8212fea9c70db0160e50a227062fe736fc8ce95
Adewale Kujore <Adewalekujore6@gmail.com>
2024-06-08 17:36:14 +0100

    assignment 7

---
+++ assignment_7/README.md
@@ -0,0 +1,33 @@
+<center> <h1>Birthday Remainder</h1> </center>
+
+### Built With
+
+-   [Materializecss](https://materializecss.com/)
+-   [Express](https://expressjs.com/)
+-   [MongoDB](https://www.mongodb.com/)
+-   [NodeMailer](https://nodemailer.com/)
+-   [Node-cron](https://www.npmjs.com/package/node-cron)
+
+## How To Use
+
+```bash
+# Clone this repository
+$ git clone https://github.com/Adewale66/alt_assignment_2.git
+
+
+# change directory
+$ cd assignment_7
+
+# Install dependencies
+$ npm install
+
+# Copy and populate env variables
+$ cat .env.example > .env
+
+# Run the app
+$ node index.js
+
+# Run cron Job
+$ node cron.js
+
+```
---
+++ assignment_7/index.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./schema");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const MONTHS = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sep",
+	"Oct",
+	"Nov",
+	"Dec",
+];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/api/birthday", async (req, res) => {
+	const { name, email, date } = req.body;
+	const [month, day] = date.split("-");
+	const birthday = new Date();
+	birthday.setMonth(MONTHS.indexOf(month));
+	birthday.setDate(day);
+
+	const userExists = await User.findOne({ email });
+
+	if (userExists) {
+		return res.sendStatus(400);
+	}
+
+	const user = new User({
+		name,
+		email,
+		birthday,
+	});
+
+	await user.save();
+	res.sendStatus(200);
+});
+
+app.get("/api/users", async (req, res) => {
+	const users = await User.find();
+	res.json(users);
+});
+
+app.listen(PORT, async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log(`Server is running on port ${PORT}`);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.error(error);
+	}
+});
